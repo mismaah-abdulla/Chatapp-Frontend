fcdfee65ae20cca9fa597e64700f30d0214e59f4
mismaah-abdulla <mismaahabdulla@gmail.com>
2019-04-10 05:49:17 +0500

    Change Name to username

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -14,7 +14,7 @@
                         v-model="name"
                         :counter="10"
                         :rules="nameRules"
-                        label="Name"
+                        label="Username"
                         required
                         ></v-text-field>
 

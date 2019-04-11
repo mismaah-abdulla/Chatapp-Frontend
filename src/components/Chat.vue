04a4e9043c831b408a63198793f162ed3ee27f4f
mismaah.abdulla <mismaahabdulla@gmail.com>
2019-04-11 08:48:34 +0500

    Chips used as chat bubbles inside tiles

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,31 +3,23 @@
             <v-flex md6>
                 <v-card>
                     <!-- <v-list dense class="pa-0" v-for="a in [,,,,]" two-line> -->
-                        <v-list two-line dense>
-                            <template v-for="(item, index) in items">
-                                <v-subheader
-                                class="primary--text headline"
-                                v-if="item.header"
-                                :key="item.header"
-                                >
-                                {{ item.header }}
-                                </v-subheader>
-
-                                <v-divider
-                                v-else-if="item.divider"
-                                :key="index"
-                                ></v-divider>
-
+                    <v-subheader
+                        class="primary--text headline">
+                        {{ header }}
+                    </v-subheader>
+                        <v-list dense>
+                            <template v-for="item in items">
                                 <v-list-tile
-                                
-                                v-else
                                 :key="item.title"
-                                avatar
-                                @click=""
-                                >
+                                avatar>
 
                                 <v-list-tile-content>
-                                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                    <v-chip>
+                                    <v-list-tile-sub-title>
+                                        <span class='text--primary'>
+                                            {{item.username}}
+                                        </span> &mdash; {{item.message}} </v-list-tile-sub-title>
+                                    </v-chip>
                                 </v-list-tile-content>
                                 </v-list-tile>
                             </template>
@@ -42,21 +34,19 @@
 export default {
     data () {
         return {
+            header: 'Main',
             items: [
-                { header: 'Main' },
                 {
-                    title: 'Brunch this weekend?',
-                    subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+                    username: 'Ali Connors',
+                    message: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
                 },
-                { divider: true, inset: true },
                 {
-                    title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-                    subtitle: "<span class='text--primary'>Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+                    username: 'Youshau',
+                    message: "Wish I could come, but I'm out of town this weekend."
                 },
-                { divider: true, inset: true },
                 {
-                    title: 'Oui oui',
-                    subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+                    username: 'Sandra Adams',
+                    message: "Do you have Paris recommendations? Have you ever been? "
                 }
             ]
         }

ac51cd20fc020a6fa16f37a80ec4b637f1debdf0
mismaah.abdulla <mismaahabdulla@gmail.com>
2019-04-11 09:48:32 +0500

    Add text field

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -20,8 +20,25 @@
                                     </v-chip>
                                 </v-list-tile-content>
                                 </v-list-tile>
+                                
                             </template>
                             </v-list>
+                        <v-text-field
+                            class="pb-3"
+                            counter="70"
+                            placeholder="Send a message"
+                            v-model="message"
+                            :append-icon="marker ? 'mdi-map-marker' : 'mdi-map-marker-off'"
+                            :append-outer-icon="message ? 'mdi-send' : 'mdi-microphone'"
+                            :prepend-icon="icon"
+                            clear-icon="mdi-close-circle"
+                            clearable
+                            type="text"
+                            @click:append="toggleMarker"
+                            @click:append-outer="sendMessage"
+                            @click:prepend="changeIcon"
+                            @click:clear="clearMessage"
+                        ></v-text-field>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -29,24 +46,62 @@
 
 <script>
 export default {
-    data () {
-        return {
-            header: 'Main',
-            items: [
-                {
-                    username: 'Ali Connors',
-                    message: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-                },
-                {
-                    username: 'Youshau',
-                    message: "Wish I could come, but I'm out of town this weekend."
-                },
-                {
-                    username: 'Sandra Adams',
-                    message: "Do you have Paris recommendations? Have you ever been? "
-                }
-            ]
-        }
+    data: () => ({
+        header: 'Main',
+        items: [
+            {
+                username: 'Ali Connors',
+                message: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+            },
+            {
+                username: 'Youshau',
+                message: "Wish I could come, but I'm out of town this weekend."
+            },
+            {
+                username: 'Sandra Adams',
+                message: "Do you have Paris recommendations? Have you ever been? "
+            }
+        ],
+        password: 'Password',
+        show: false,
+        marker: true,
+        iconIndex: 0,
+        icons: [
+            'mdi-emoticon',
+            'mdi-emoticon-cool',
+            'mdi-emoticon-dead',
+            'mdi-emoticon-excited',
+            'mdi-emoticon-happy',
+            'mdi-emoticon-neutral',
+            'mdi-emoticon-sad',
+            'mdi-emoticon-tongue'
+        ]
+        }),
+    computed: {
+      icon () {
+        return this.icons[this.iconIndex]
+      }
+    },
+
+    methods: {
+      toggleMarker () {
+        this.marker = !this.marker
+      },
+      sendMessage () {
+        this.resetIcon()
+        this.clearMessage()
+      },
+      clearMessage () {
+        this.message = ''
+      },
+      resetIcon () {
+        this.iconIndex = 0
+      },
+      changeIcon () {
+        this.iconIndex === this.icons.length - 1
+          ? this.iconIndex = 0
+          : this.iconIndex++
+      }
     }
 }
 </script>
